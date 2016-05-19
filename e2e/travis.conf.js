38cc62e69a27f33f02ba4e2c02b06549ebf2dce9
Przemek <mrnz@wp.pl>
2016-05-19 19:32:49 +0100

    travis fix 5

---
+++ e2e/travis.conf.js
@@ -0,0 +1,19 @@
+var exac = require('child_process').exec;
+
+exports.config = {
+	framework: 'custom',
+	frameworkPath: require.resolve('protractor-cucumber-framework'),
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['./features/*.feature'],
+
+  capabilities: {
+  	browserName: 'chrome'
+  },
+
+  cucumberOpts: {
+  	require:'./step_definitions/*.js'
+  },
+  onPrepare: function(){
+  	console.log('TEST Preparation');
+  }
+};
