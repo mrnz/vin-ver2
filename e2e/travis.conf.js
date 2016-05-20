87ec9f839a6df190358fe4870716a2df5cff248a
Przemek <mrnz@wp.pl>
2016-05-20 10:03:22 +0100

    travis fix 14

--- e2e/travis.conf.js
+++ e2e/travis.conf.js
@@ -3,12 +3,13 @@ var exac = require('child_process').exec;
 exports.config = {
 	framework: 'custom',
 	frameworkPath: require.resolve('protractor-cucumber-framework'),
-
+  seleniumAddress: 'http://172.17.7.99:35303/wd/hub',
   specs: ['./features/*.feature'],
 
   capabilities: {
   	browserName: 'firefox'
   },
+  directConnect: true,
   cucumberOpts: {
   	require:'./step_definitions/*.js'
   },
