7cf8a34f7d9223ab4ee781cb959cac6b6cc99b91
Przemek <mrnz@wp.pl>
2016-05-20 11:10:25 +0100

    travis fix 19

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,18 +28,45 @@ module.exports = function(grunt){
 	      command: 'r.js -o www/js/main.build.js'
 	    }
 	    // another: 'ls ./src' // shorthand
-	  }
+	  },
+
+
+		connect: {
+		  server: {
+		    options: {
+		      port: 8000,
+		      hostname: '*',
+		      base: './',
+		      keepalive: true,
+
+		      onCreateServer: function(server, connect, options) {
+		        	console.log(server)
+		        	console.log('--------------')
+		        	console.log(connect)
+		        	console.log('--------------')
+		        	console.log(options)
+		        	console.log('--------------')
+		      }
+		    }
+		  }
+		},
+		exec: {
+		  start_server: {
+		    command: 'grunt connect &'
+		  }
+		}
 
 
 
 	});
-
+	
 	grunt.loadNpmTasks('grunt-protractor-runner');
 	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-exec');
 
-
-	grunt.registerTask('e2e-local', ["protractor:e2e_local"]);
-	grunt.registerTask('e2e-travis', ["protractor:e2e_travis"]);
+	grunt.registerTask('e2e-local', ["exec","protractor:e2e_local"]);
+	grunt.registerTask('e2e-travis', ["exec","protractor:e2e_travis"]);
 	grunt.registerTask('build', ["shell:target"]);
 
 
