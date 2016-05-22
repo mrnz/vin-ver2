ec91eb4d1702165f4c8725708a0b0e37d4f21824
Przemek <mrnz@wp.pl>
2016-05-22 17:44:20 +0100

    travis fix 32

--- e2e/step_definitions/generic-steps.js
+++ e2e/step_definitions/generic-steps.js
@@ -11,23 +11,38 @@ module.exports = function () {
 	
 	this.Given(/^Start server$/, function (next) {
     
-  // 	connect().use(serveStatic("./")).listen(37940, function(){
+  // 	connect().use().listen(37940, function(){
 		// 	next();
 		// })	
-    connect().use(function middleware1(req, res, next) {
-      res.writeHead(301, {Location: 'https://angularjs.org'});
-      res.end();
-      console.log('I see request');
-    }).listen(8098, function(){
+
+
+
+    connect()
+    .use(serveStatic("./"))
+    // .use(function middleware1(req, res, next) {
+    //   res.writeHead(301, {Location: 'https://angularjs.org'});
+    //   res.end();
+    //   console.log('I see request');
+    // })
+    .listen(8098, function(){
       console.log('server has started');
       next();
-    });    
+    }); 
+
+    // connect().use(function middleware1(req, res, next) {
+    //   res.writeHead(301, {Location: 'https://angularjs.org'});
+    //   res.end();
+    //   console.log('I see request');
+    // }).listen(8098, function(){
+    //   console.log('server has started');
+    //   next();
+    // });    
     
   });
 
   this.Then(/^Open app page$/,{timeout: 20000},  function (next) {
    
-    browser.get(baseURL).then(function () {
+    browser.get(baseURL+'www').then(function () {
       
       browser.waitForAngular();
 
